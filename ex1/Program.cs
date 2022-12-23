9a41d60a1bd3bfec4948a2007cc137d92befc785
l.orlova <igimon@mail.ru>
2022-12-24 00:08:05 +0300

    Create homework9

---
+++ ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+const int N = 8;
+
+void WriteNumbers (int N)
+{
+    if(N == 1) System.Console.Write( 1);
+    else 
+    {
+        System.Console.Write(N+", ");
+        WriteNumbers(N-1);
+    }
+}
+
+WriteNumbers(N);
---
+++ ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 4, N = 8;
+
+int GetSum (int M, int N)
+{
+    if(N == M) return M;
+    else return N + GetSum(M, N-1);
+}
+
+int result = GetSum(M, N);
+System.Console.WriteLine(result);
